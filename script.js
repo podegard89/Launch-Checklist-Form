d35c33f18a877daeceef3841dffdd9433d03e0a9
Pearce Odegard <pearceodegard@gmail.com>
2020-09-10 00:59:08 -0500

    Commit with for loops that doesn't work

--- script.js
+++ script.js
@@ -11,3 +11,31 @@
 </ol>
 <img src="${}">
 */
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+   let inputNames = [document.querySelector("input[name=pilotName]"), document.querySelector("input[name=copilotName]")];
+   let inputNumbers = [document.querySelector("input[name=fuelLevel]"), document.querySelector("input[name=cargoMass]")];
+   
+   form.addEventListener("submit", function(event) {
+      for(i = 0; i < inputNames.length; i++) {
+         if(inputNames[i].value === "") {
+            alert("All fields required");
+            event.preventDefault();
+         } else if (typeof inputNames[i].value !== string) {
+            alert("Invalid input. Please provide valid input.");
+            event.preventDefault();
+         }
+      }
+
+      for(i = 0; i < inputNumbers.length; i++) {
+         if(inputNumbers[i].value === "") {
+            alert("All fields required");
+            event.preventDefault();
+         } else if (typeof Number(inputNumbers[i].value) !== number) {
+            alert("Invalid input. Please provide valid input.");
+            event.preventDefaul();
+         }
+      }
+
+      })
+   });
